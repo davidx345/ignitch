ce224dab2e1e09d4d72437a5d5a1798ea572efe6
davidx345 <davidayo2603@gmail.com>
2025-09-07 21:26:02 +0100

    jkk

--- backend/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-# This file makes the schemas directory a Python package
-# Temporarily commented out to fix circular import issue
-# from schemas import UserCreate, UserResponse, UserBase
-
-# __all__ = ["UserCreate", "UserResponse", "UserBase"]
